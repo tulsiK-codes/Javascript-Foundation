04207cdc86215692c4aa55b98bdc475c4fe36aaa
tulsiK-codes <tulsikp2004@gmail.com>
2026-04-02 18:52:53 +0530

    primitive datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs,not browser
+
+/* 
+console.log(3
++ 3) // code readability should be high
+*/
+
+// console.log("Tulsi");
+
+//DATATYPES
+let userName = "tulsi"; //string
+let age = 22 //number
+let isLoggedIn = false //boolean
+let state //undefined
+
+// console.log(typeof name); deprecated because TypeScript:Sees name as a conflict with global window.name
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value not assigned
+// symbol => unique
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
