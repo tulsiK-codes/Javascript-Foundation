4808c3fa57d16fb38ef9ad795daf363344e941fa
tulsiK-codes <tulsikp2004@gmail.com>
2026-04-05 14:01:32 +0530

    created datesInjs file

---
+++ 01_Basics/07_dateinJs.js
@@ -0,0 +1,7 @@
+//Dates (Note: Now Temporal API is available, but its still not supported by nodejs, so we arelearing about dates here!)
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
