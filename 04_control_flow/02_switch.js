e97423002ce6a69b3005ae286a7425d4d78d69bc
tulsiK-codes <tulsikp2004@gmail.com>
2026-04-07 21:06:16 +0530

    switch case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,60 @@
+//Syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month ="april"
+
+switch (month) {
+    case "jan":
+        console.log(`Month is ${month}`)
+        break;
+    case "feb":
+        console.log(`Month is ${month}`)
+        break;
+    case "march":
+        console.log(`Month is ${month}`)
+        break;
+    case "april":
+        console.log(`Month is ${month}`)
+        break;
+    
+    default:
+        console.log("Enter another month");
+        
+        break;
+}
+
+
+const day = 3
+switch (day) {
+    case 1:
+        console.log("Its Sunday");        
+        break;
+    case 2:
+        console.log("Its Monday");        
+        break;
+    case 3:
+        console.log("Its Tuesday");        
+        break;
+    case 4:
+        console.log("Its Wednesday");        
+        break;
+    case 5:
+        console.log("Its Thursday");        
+        break;
+    case 6:
+        console.log("Its Friday");        
+        break;
+    case 7:
+        console.log("Its Saturday");        
+        break;
+
+    default:
+        break;
+}
