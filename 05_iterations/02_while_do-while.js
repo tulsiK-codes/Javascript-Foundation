6369a9f4ef696f99a2c98c7cfbdd0b8553085a65
tulsiK-codes <tulsikp2004@gmail.com>
2026-04-07 23:18:39 +0530

    while & do-while loops

---
+++ 05_iterations/02_while_do-while.js
@@ -0,0 +1,19 @@
+let i = 1
+while(i <= 10){
+    // console.log(i);
+    i = i + 2
+}
+
+let arr = ["apple", "mango", "orange"]
+let index = 0
+while (index < arr.length) {
+    // console.log(arr[index]);
+    index++
+}
+
+//do-while
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
