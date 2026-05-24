e13b92a6391ab633eab9cadb378e6fc4f90ad88c
tulsiK-codes <tulsikp2004@gmail.com>
2026-05-24 15:23:06 +0530

    The step by step flow of execution added

--- 10_classes_and_oops/10_class_get_set.js
+++ 10_classes_and_oops/10_class_get_set.js
@@ -27,15 +27,12 @@ console.log(myUser.password); //456 we got access to the password
 console.log(myUser.email);
 
 
-
-
-
-
-
-
-
-
-
+/*
+When you run const hitesh = new User("h@hitesh.ai", "abc"):
+1. The Constructor Fires: JavaScript runs this.email = "h@hitesh.ai";.
+2. The Setter is Triggered: Because a setter named email exists, JavaScript instantly jumps down to set email(value).
+3. The Data is Saved Safely: Inside the setter, it executes this._email = value;. This creates the underscore version (_email) in memory and stores "h@hitesh.ai" inside it.4. The Loop is Broken: Because _email has no setter attached to it, the process stops cleanly.
+*/
 
 
 /* Similarly there are many scenarios where we dont want to allow access to some properties like password, or show message like "Cannot show password" or encrypted password. For these purposes, getters and setters are used */
